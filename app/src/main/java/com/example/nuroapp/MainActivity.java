4f6e39b17e87c2e43efbaa364781456b8ddd17b4
Karim Mrad <mrad.kzm@gmail.com>
2021-11-29 20:31:33 +0200

    Fixed an issue.

--- app/src/main/java/com/example/nuroapp/MainActivity.java
+++ app/src/main/java/com/example/nuroapp/MainActivity.java
@@ -5,14 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
-import ir.shahinsoft.graphdraw.GraphView;
-import ir.shahinsoft.graphdraw.model.Graph;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_graph_draw);
+        setContentView(R.layout.activity_main);
     }
 }
