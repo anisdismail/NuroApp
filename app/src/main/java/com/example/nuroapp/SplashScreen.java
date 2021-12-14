b750b136b384cfdcddf8df4de95dd1e233b216cb
Karim Mrad <mrad.kzm@gmail.com>
2021-12-14 22:23:07 +0200

    Blah blah

--- app/src/main/java/com/example/nuroapp/SplashScreen.java
+++ app/src/main/java/com/example/nuroapp/SplashScreen.java
@@ -6,14 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 3000;
+
+    ImageView logoImage;
+    TextView nuroText;
+
+    Animation sideAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         ActionBar actionBar = getSupportActionBar();
+
+        logoImage = findViewById(R.id.logo_splash);
+        nuroText = findViewById(R.id.nuro_text);
+
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+
+        logoImage.setAnimation(sideAnim);
+        nuroText.setAnimation(bottomAnim);
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
@@ -22,6 +44,6 @@ public class SplashScreen extends AppCompatActivity {
                 SplashScreen.this.startActivity(mainIntent);
                 SplashScreen.this.finish();
             }
-        }, 3000);
+        }, SPLASH_TIMER);
     }
 }
