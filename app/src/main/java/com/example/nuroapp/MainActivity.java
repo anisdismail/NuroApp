15dd4a6886472a145cf4ebc11aeaef7bb205a04a
Karim Mrad <mrad.kzm@gmail.com>
2021-12-16 09:50:18 +0200

    fixed bugs

--- app/src/main/java/com/example/nuroapp/MainActivity.java
+++ app/src/main/java/com/example/nuroapp/MainActivity.java
@@ -153,6 +153,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed(){
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
