b27824b14a7f8f201423f36a7e692e658c6f36ce
Abdelbasset-Jalaleddine <abdelbasset@dotcomlb.com>
2021-12-16 04:12:11 +0200

    Sign in and Sign up are working now!

--- app/src/main/java/com/example/nuroapp/MainActivity.java
+++ app/src/main/java/com/example/nuroapp/MainActivity.java
@@ -25,6 +25,11 @@ import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,8 +42,10 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     FirebaseUser fbUser;
     FirebaseAuth mAuth;
+    DatabaseReference dbReference;
     ImageView out;
     Button MLWorkflowBtn;
+    String firstName;
 
 
 
@@ -48,7 +55,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         fbUser = mAuth.getCurrentUser();
-        textView.setText("Welcome!");
+        dbReference = FirebaseDatabase.getInstance().getReference("Users");
+        dbReference.child(fbUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    firstName = userProfile.firstName;
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        textView.setText("Welcome! Ready to learn?");
         out = findViewById(R.id.sign_out);
         MediaPlayer clickSound = MediaPlayer.create(this,R.raw.click);
        actionBar = getSupportActionBar();
@@ -153,7 +177,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void logOut(View view){
-
+        mAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
     }
 
 }
